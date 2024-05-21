3faf983cf811231d572b12aae7a17da2c0afe85d
yuxiaoyuan0406 <973971990@qq.com>
2024-05-21 11:19:22 +0800

    redefine electrical forces

--- spr_dmp_simpy.py
+++ spr_dmp_simpy.py
@@ -13,23 +13,22 @@ def external_force(t):
 if __name__ == '__main__':
     env = simpy.Environment(0)
     
+    runtime = 1.
+    dt = 1e-6
+
     initial_state = np.array([0., 0.])
     spring_system = SpringDampingSystem(
         env=env,
         mass=7.45e-7,
         spring_coef=5.623,
         damping_coef=4.95e-6,
-        area=0,
-        gap=0,
-        v_ref=2.5,
         initial_state=initial_state,
-        input=external_force
+        runtime=runtime,
+        dt=dt,
+        input_force=external_force,
     )
 
-    runtime = 1.
-    dt = 1e-6
-
-    env.process(spring_system.run(runtime, dt))
+    # env.process(spring_system.run(runtime, dt))
     with tqdm(total=int(runtime/dt), desc='Running simulation') as pbar:
         while env.now < runtime:
             env.run(until=env.now + dt)
