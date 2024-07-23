f6364733fe1e15ea68d8b602d26263582edcd363
yuxiaoyuan0406 <973971990@qq.com>
2024-07-23 15:08:03 +0800

    add an wrapper for vectorizing
    Wrap any single value
    function(float to float for example) to behave
    like a numpy function such as `np.sin`, which
    returns an array when given an array and returns
    a scalar value when given a scalar.

--- util/util.py
+++ util/util.py
@@ -5,6 +5,8 @@ Some useful utilities.
 import json
 from datetime import datetime
 import os
+from functools import wraps
+import numpy as np
 
 now = datetime.now()
 formatted_date_time = now.strftime('%Y%m%d-%H%M%S')
@@ -22,3 +24,34 @@ def save(file_name: str, data: dict):
     with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2)
         f.close()
+
+
+def vectorize(func):
+    '''
+    A vectorizing function to wrap any single value
+    function(float to float for instance) to behave
+    like a numpy function such as `np.sin`, which
+    returns an array when given an array and returns
+    a scalar value when given a scalar.
+    '''
+
+    @wraps(func)
+    def wrapper(x):
+        if np.isscalar(x):
+            return func(x)
+        else:
+            vectorized_func = np.vectorize(func)
+            return vectorized_func(x)
+
+    return wrapper
+
+
+if __name__ == '__main__':
+    @vectorize
+    def unit_pulse(x):
+        if x == 0:
+            return 1.
+        return 0.
+
+    t = np.linspace(0, 1, int(1e6), endpoint=False)
+    print(unit_pulse(t).dtype)
