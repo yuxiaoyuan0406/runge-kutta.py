947ec9b0a21f32531a3c0aaedf9d9935d18f4931
yuxiaoyuan0406 <973971990@qq.com>
2025-07-09 11:20:33 +0800

    添加时间序列和频率响应图的默认绘图函数

--- util/plot.py
+++ util/plot.py
@@ -4,6 +4,42 @@ Simple plot.
 import numpy as np
 import matplotlib.pyplot as plt
 
+def default_time_plot_fig():
+    """
+    Creates and returns a default matplotlib figure and axis for time series plots.
+
+    The figure is sized to 16x9 inches, with grid enabled, legend positioned at the upper right,
+    and the x-axis labeled as 'Time [s]'.
+
+    Returns:
+        tuple: A tuple containing the matplotlib Figure and Axes objects (fig_time, ax_time).
+    """
+    fig_time, ax_time = plt.subplots(figsize=(16, 9))
+    ax_time.grid(True)
+    ax_time.legend(loc='upper right')
+    ax_time.set_xlabel('Time [s]')
+    return fig_time, ax_time
+
+def default_freq_plot_fig():
+    """
+    Creates a default matplotlib figure for frequency response plots with two subplots: power and phase.
+
+    Returns:
+        tuple: A tuple containing the figure object and a tuple of axes (ax_power, ax_phase).
+            - fig_freq (matplotlib.figure.Figure): The created figure.
+            - (ax_power, ax_phase) (tuple of matplotlib.axes.Axes): The axes for power and phase plots.
+    """
+    fig_freq, (ax_power, ax_phase) = plt.subplots(2, 1, figsize=(16, 9), sharex=True)
+    ax_power.grid(True)
+    ax_phase.grid(True)
+    ax_power.set_xscale('log')
+    ax_phase.set_xscale('log')
+    # ax_power.set_ylabel('dB')
+    ax_phase.set_xlabel('Frequency [Hz]')
+    ax_power.legend(loc='upper right')
+    ax_phase.legend(loc='upper right')
+    return fig_freq, (ax_power, ax_phase)
+    
 
 def plot(
     x_data: np.ndarray,  # 自变量数据
